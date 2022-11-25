f19c07ae6c4b21da08ac3d00166bc97f2bbf7a49
Ashutosh-Bhardwaj-26 <68807593+Ashutosh-Bhardwaj-26@users.noreply.github.com>
2022-11-25 23:47:02 +0530

    final again 8

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import base64
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import pandas_datareader as data 
 import yfinance as yf
 from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
