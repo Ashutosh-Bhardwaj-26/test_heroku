6d047a5e1c9c084fd55e435ab99eb90e9bf1a4cc
Ashutosh-Bhardwaj-26 <68807593+Ashutosh-Bhardwaj-26@users.noreply.github.com>
2022-11-25 23:25:55 +0530

    final again 3

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def index():
 
     #ticker = request.args['ticker']
 
-    df = yf.Ticker(AAPL).history(period='max', # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+    df = yf.Ticker('AAPL').history(period='max', # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
                                    interval='1d', # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
                                    actions=False)
     #ticker = request.args['ticker']
